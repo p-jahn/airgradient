34c6b20851b6fbae536730307bedea4c199da572
Patrick Jahn <33724206+p-jahn@users.noreply.github.com>
2023-03-26 16:48:15 +0200

    reorder and use single-shot particle readings

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <ESP8266WiFi.h>
 #include <U8g2lib.h>
 #include <WiFiClient.h>
+
 /*
 BUILD PARAMETER DEPENDENT SETUP
 */
@@ -28,6 +29,7 @@ BUILD PARAMETER DEPENDENT SETUP
     #endif
 const auto pushFrequency = 60000U;
 uint32_t lastPush;
+void pushInfluxMetrics(uint32_t now);
 #endif
 
 #ifdef WIFI_ENABLED
@@ -37,6 +39,7 @@ uint32_t lastPush;
     #ifndef WIFI_PASSWORD
         #error "WIFI_PASSWORD is required when WIFI_ENABLED"
     #endif
+void setupWifi();
 #endif
 
 #ifndef DEVICE_NAME
@@ -49,6 +52,7 @@ uint32_t lastPush;
     #endif
 auto server = ESP8266WebServer(SERVER_PORT);
 String labels;
+void setupServer();
 #endif
 /*
 /BUILD PARAMETER DEPENDENT SETUP
@@ -60,12 +64,49 @@ uint32_t lastFetch;
 String temp;
 String rh;
 String co2;
-String pm1;
-String pm2;
-String pm10;
+String pm1 = "0";
+String pm2 = "0";
+String pm10 = "0";
 
 auto sensors = AirGradient();
 auto display = U8G2_SSD1306_64X48_ER_1_HW_I2C(/* rotation=*/U8G2_R0, /* reset=*/U8X8_PIN_NONE);
+void displayThreeLines(const char* l1, const char* l2, const char* l3);
+void fetchSensorsAndDisplay(uint32_t now);
+
+void setup() {
+    Serial.begin(9600);
+
+    display.begin();
+
+    displayThreeLines("CO2 init", "", "");
+    sensors.CO2_Init();
+    displayThreeLines("CO2 done", "PMS init", "");
+    sensors.PMS_Init();
+    displayThreeLines("CO2 done", "PMS done", "TRH init");
+    sensors.TMP_RH_Init();
+    displayThreeLines("CO2 done", "PMS done", "TRH done");
+
+#ifdef WIFI_ENABLED
+    setupWifi();
+#endif
+
+#ifdef SERVER_ENABLED
+    setupServer();
+#endif
+
+    fetchSensorsAndDisplay(millis());
+}
+
+void loop() {
+    auto t = millis();
+    fetchSensorsAndDisplay(t);
+#ifdef SERVER_ENABLED
+    server.handleClient();
+#endif
+#ifdef PUSH_ENABLED
+    pushInfluxMetrics(t);
+#endif
+}
 
 void displayThreeLines(const char* l1, const char* l2, const char* l3) {
     display.firstPage();
@@ -102,16 +143,12 @@ void fetchSensorsAndDisplay(uint32_t now) {
     rh = String(tmpRh.rh);
     co2 = String(sensors.getCO2_Raw());
 
-    /*
-    TODO:
-      Modify lib to get all particulate data in one shot.
-      Currently it will reach out to the sensor for each
-      value, although all values are contained in the
-      singular sensors respone.
-    */
-    pm1 = String(sensors.getPM1_Raw());
-    pm2 = String(sensors.getPM2_Raw());
-    pm10 = String(sensors.getPM10_Raw());
+    auto parts = sensors.getAtmosphericParticles();
+    if (parts.error == PMS_NO_ERROR) {
+        pm1 = String(parts.PM1_0);
+        pm2 = String(parts.PM2_5);
+        pm10 = String(parts.PM10_0);
+    }
 
     displayThreeLines(("C " + temp).c_str(),
                       ("RH " + rh).c_str(),
@@ -131,18 +168,18 @@ void pushInfluxMetrics(uint32_t now) {
       We should(TM) set the accepted fingerprint here, like in
       setFingerprint(uint8_t[]). But then we would have to renew it when the
       certificate changes because we can't afford something like a trust store
-      with our teeny tiny memory budget. Also, keeping track of the fingerprint
-      sounds very stressful - so lets join the ~internet of shit~ :/
+      with our teeny tiny memory budget.
+      Insecure means that we don't verifiy who we are talking to. In theory,
+      someone might take over the connection and capture our values. Then
+      they might know when no one is home (e.g. low CO2) or similar...
 
-      Maybe, somewhere in the future, insecure will be the fallback if we cannot
-      find a matching fingerprint. We are already pushing stuff, we have a
-      display and a (eye searing bright) LED on board, so we could go into ALERT
-      mode if this is the case. The boss then has to update the firmware - maybe
-      not so bad and probably better that doing the
-      possibly-privacy-data-leaking thing we do now. (you know: "Hoody-Hackerboy
-      found out that the CO2 is very constant, maybe no one is at home and one
-      could take some stuff that doesn't belong to them without being
-      disturbed..."
+      TODO:
+        Implement a more secure soltution.
+        Maybe use the display/LED to indicate when there is an issue with the
+        fingerprint.
+        Maybe, additionally, allow falling back to `insecure` so metrics are
+        still pushed.
+        Maybe allow online updating via http push.
       )
     */
     connection.setInsecure();
@@ -188,6 +225,40 @@ void setupWifi() {
 #endif
 
 #ifdef SERVER_ENABLED
+void setupLabels();
+void appendPromGauge(String& current, const char* name, const char* help, const char* value);
+void serveMetrics();
+
+void setupServer() {
+    setupLabels();
+
+    server.on("/", serveMetrics);
+    server.on("/metrics", serveMetrics);
+    server.onNotFound(std::function<void(void)>(
+        []() { server.send(404, "text/plain", "not found"); }));
+    server.onFileUpload(std::function<void(void)>(
+        []() { server.send(405, "text/plain", "not allowed"); }));
+
+    server.begin();
+
+    displayThreeLines("serving metrics", "on port", String(SERVER_PORT).c_str());
+    delay(1000);
+}
+
+auto promMetrics = String();
+
+void serveMetrics() {
+    promMetrics.clear();
+    appendPromGauge(promMetrics, "temperature", "Temperature in °C", temp.c_str());
+    appendPromGauge(promMetrics, "humidity", "Realative humidity in %", rh.c_str());
+    appendPromGauge(promMetrics, "CO2", "CO2 concentraion in PPM", co2.c_str());
+    appendPromGauge(promMetrics, "particulate_matter_1", "particle count (1µm and below, atmospheric environment) in µg/m³", pm1.c_str());
+    appendPromGauge(promMetrics, "particulate_matter_2_5", "particle count (1µm to 2.5µm and below, atmospheric environment) in µg/m³", pm2.c_str());
+    appendPromGauge(promMetrics, "particulate_matter_10", "particle count (2.5µm to 10µm, atmospheric environment) in µg/m³", pm10.c_str());
+
+    server.send(200, "text/plain;charset=utf-8", promMetrics);
+}
+
 void setupLabels() {
     char extra[64];
     #ifdef LOCATION
@@ -214,69 +285,4 @@ void appendPromGauge(String& current, const char* name, const char* help, const 
     }
     current += buf;
 }
-
-auto promMetrics = String();
-
-void serveMetrics() {
-    promMetrics.clear();
-    appendPromGauge(promMetrics, "temperature", "Temperature in °C", temp.c_str());
-    appendPromGauge(promMetrics, "humidity", "Realative humidity in %", rh.c_str());
-    appendPromGauge(promMetrics, "CO2", "CO2 concentraion in PPM", co2.c_str());
-    appendPromGauge(promMetrics, "particulate_matter_1", "particle count (1µm and below, atmospheric environment) in µg/m³", pm1.c_str());
-    appendPromGauge(promMetrics, "particulate_matter_2_5", "particle count (1µm to 2.5µm and below, atmospheric environment) in µg/m³", pm2.c_str());
-    appendPromGauge(promMetrics, "particulate_matter_10", "particle count (2.5µm to 10µm, atmospheric environment) in µg/m³", pm10.c_str());
-
-    server.send(200, "text/plain;charset=utf-8", promMetrics);
-}
-
-void setupServer() {
-    setupLabels();
-
-    server.on("/", serveMetrics);
-    server.on("/metrics", serveMetrics);
-    server.onNotFound(std::function<void(void)>(
-        []() { server.send(404, "text/plain", "not found"); }));
-    server.onFileUpload(std::function<void(void)>(
-        []() { server.send(405, "text/plain", "not allowed"); }));
-
-    server.begin();
-
-    displayThreeLines("serving metrics", "on port", String(SERVER_PORT).c_str());
-    delay(1000);
-}
 #endif
-
-void setup() {
-    Serial.begin(9600);
-
-    display.begin();
-
-    displayThreeLines("CO2 init", "", "");
-    sensors.CO2_Init();
-    displayThreeLines("CO2 done", "PMS init", "");
-    sensors.PMS_Init();
-    displayThreeLines("CO2 done", "PMS done", "TRH init");
-    sensors.TMP_RH_Init(0x44);
-    displayThreeLines("CO2 done", "PMS done", "TRH done");
-
-#ifdef WIFI_ENABLED
-    setupWifi();
-#endif
-
-#ifdef SERVER_ENABLED
-    setupServer();
-#endif
-
-    fetchSensorsAndDisplay(millis());
-}
-
-void loop() {
-    auto t = millis();
-    fetchSensorsAndDisplay(t);
-#ifdef SERVER_ENABLED
-    server.handleClient();
-#endif
-#ifdef PUSH_ENABLED
-    pushInfluxMetrics(t);
-#endif
-}
